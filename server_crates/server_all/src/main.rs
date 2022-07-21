94170fe8baf93dd7f1312cd02d963593ab0be309
Jörn Heinemann <joernheinemann@gmx.de>
2022-07-21 23:38:59 +0200

    added a full server for all crates

---
+++ server_crates/server_all/src/main.rs
@@ -0,0 +1,23 @@
+use std::env;
+
+use server_api::{rest_routes, start};
+
+/**
+merge every server into one
+*/
+#[tokio::main]
+pub async fn main()
+{
+	start().await;
+	//routes from the rest api
+	let mut router = rest_routes();
+
+	//the static files from the dash board
+	server_dashboard::routes(&mut router);
+
+	let addr = format!("{}:{}", env::var("SERVER_HOST").unwrap(), env::var("SERVER_PORT").unwrap())
+		.parse()
+		.unwrap();
+
+	rustgram::start(router, addr).await;
+}
