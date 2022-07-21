e6e0a72bac3081bb38770e9b2c4be060cad09e51
Jörn Heinemann <joernheinemann@gmx.de>
2022-07-21 20:07:48 +0200

    added rust gram route builder

--- server_api_route_builder/src/main.rs
+++ server_api_route_builder/src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-	println!("Hello, world!");
+use rustgram::route_parser;
+
+fn main()
+{
+	route_parser::start("server_api/routes.yml".to_string(), "server_api/src/routes.rs".to_string());
 }
