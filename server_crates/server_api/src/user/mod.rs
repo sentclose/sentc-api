34ccaa093b1a6677f6b1ed49ff1ef89970d87ca0
Jörn Heinemann <joernheinemann@gmx.de>
2022-07-22 10:29:14 +0200

    move controller into the mod

--- server_crates/server_api/src/user/mod.rs
+++ server_crates/server_api/src/user/mod.rs
@@ -1,3 +1,42 @@
-pub(crate) mod user_controller;
 mod user_entities;
 mod user_model;
+
+use rustgram::Request;
+use sentc_crypto_common::user::{RegisterData, UserIdentifierAvailableServerOutput};
+
+use crate::core::api_err::HttpErr;
+use crate::core::input_helper::{bytes_to_json, get_raw_body, json_to_string};
+
+pub(crate) async fn exists(_req: Request) -> Result<String, HttpErr>
+{
+	let user_id = "058ed2e6-3880-4a7c-ab3b-fd2f5755ea43"; //get this from the url param
+
+	let exists = user_model::check_user_exists(user_id).await?;
+
+	let out = UserIdentifierAvailableServerOutput {
+		user_identifier: user_id.to_string(),
+		available: exists,
+	};
+
+	json_to_string(&out)
+}
+
+pub(crate) async fn register(req: Request) -> Result<String, HttpErr>
+{
+	//load the register input from the req body
+	let body = get_raw_body(req).await?;
+
+	let _register_input: RegisterData = bytes_to_json(&body)?;
+
+	Ok(format!("done"))
+}
+
+pub(crate) async fn get(_req: Request) -> Result<String, HttpErr>
+{
+	let user_id = "abc"; //get this from the url param
+
+	//
+	let user = user_model::get_user(user_id).await?;
+
+	json_to_string(&user)
+}
