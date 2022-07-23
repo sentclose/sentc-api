29e10d40f35ecf3dfee67d7c6dabe524abae20b0
Jörn Heinemann <joernheinemann@gmx.de>
2022-07-23 14:08:11 +0200

    added dummy customer register and app register

--- server_crates/server_api/src/customer/customer_entities.rs
+++ server_crates/server_api/src/customer/customer_entities.rs
@@ -8,18 +8,20 @@ Data which is used to identify the customers app requests.
 Cache this data:
 - the valid jwt keys
 - the general app data
+
+Only internal values from the db
 */
-#[derive(Serialize, Deserialize)]
 pub struct CustomerAppData
 {
 	pub app_data: CustomerAppDataGeneral,
-	pub jwt_data: Vec<CustomerAppJwt>,
+	pub jwt_data: Vec<CustomerAppJwt>, //use the newest jwt data to create a jwt, but use the old one to validate the old jwt.
 }
 
 /**
 This values can only be exists once
+
+Only internal values from the db
 */
-#[derive(Serialize, Deserialize)]
 pub struct CustomerAppDataGeneral
 {
 	pub customer_id: CustomerId,
@@ -36,8 +38,9 @@ The key data for creating jwt for the app.
 When customer logged in in the dashboard, sentc internal keys are used.
 
 It is possible to have multiple valid jwt keys.
+
+Only internal values from the db
 */
-#[derive(Serialize, Deserialize)]
 pub struct CustomerAppJwt
 {
 	pub jwt_key_id: SignKeyPairId,
@@ -49,4 +52,36 @@ pub struct CustomerRegisterData
 {
 	pub email: String,
 	pub register_data: RegisterData,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct CustomerRegisterOutput
+{
+	pub customer_id: String,
+}
+
+/**
+When creating multiple jwt keys for this app
+*/
+#[derive(Serialize, Deserialize)]
+pub struct CustomerAppJwtRegisterOutput
+{
+	pub customer_id: CustomerId,
+	pub app_id: AppId,
+	pub jwt_verify_key: String,
+	pub jwt_sign_key: String,
+	pub jwt_alg: String, //should be ES384 for now
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct CustomerAppRegisterOutput
+{
+	pub customer_id: CustomerId,
+	pub app_id: AppId,
+
+	//don't show this values in te normal app data
+	pub secret_token: String,
+	pub public_token: String,
+
+	pub jwt_data: CustomerAppJwtRegisterOutput,
 }
--- server_crates/server_api/src/customer/mod.rs
+++ server_crates/server_api/src/customer/mod.rs
@@ -1,6 +1,54 @@
 use rustgram::Request;
 
+use crate::core::api_err::HttpErr;
+use crate::core::input_helper::{bytes_to_json, get_raw_body};
+use crate::customer::customer_entities::{CustomerAppJwtRegisterOutput, CustomerAppRegisterOutput, CustomerRegisterData};
+use crate::user::jwt::create_jwt_keys;
+
 //TODO create new customer, delete customer, see how many active user per customer, valid customer data and valid customer token
 pub(crate) mod customer_entities;
 
-pub(crate) async fn create(_req: Request) {}
+pub(crate) async fn create(req: Request) -> Result<String, HttpErr>
+{
+	let body = get_raw_body(req).await?;
+
+	let _register_data: CustomerRegisterData = bytes_to_json(&body)?;
+
+	//check the email if it is a real email. send an email to complete the registration
+
+	Ok(format!("done"))
+}
+
+pub(crate) async fn done_create(_req: Request) -> Result<String, HttpErr>
+{
+	//create the jwt keys when email was ok
+
+	Ok(format!("done"))
+}
+
+pub(crate) async fn create_app(_req: Request) -> Result<String, HttpErr>
+{
+	//1. create the first jwt keys
+	let (jwt_sign_key, jwt_verify_key, alg) = create_jwt_keys()?;
+
+	//2. create an new app (with new secret_token and public_token)
+	//3. save both tokens hashed in the db
+	let customer_id = "abc".to_string();
+	let app_id = "dfg".to_string();
+
+	let _customer_app_data = CustomerAppRegisterOutput {
+		customer_id: customer_id.to_string(),
+		app_id: app_id.to_string(),
+		secret_token: "".to_string(),
+		public_token: "".to_string(),
+		jwt_data: CustomerAppJwtRegisterOutput {
+			customer_id,
+			app_id,
+			jwt_verify_key,
+			jwt_sign_key,
+			jwt_alg: alg.to_string(),
+		},
+	};
+
+	Ok(format!("done"))
+}
