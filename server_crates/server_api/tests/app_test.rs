49e0f064fa2773f3e32fe03da3e231543d776d42
Jörn Heinemann <joernheinemann@gmx.de>
2022-07-26 21:51:54 +0200

    added test for app token renew

--- server_crates/server_api/tests/app_test.rs
+++ server_crates/server_api/tests/app_test.rs
@@ -1,6 +1,6 @@
 use reqwest::StatusCode;
 use sentc_crypto_common::ServerOutput;
-use server_api::{AppRegisterInput, AppRegisterOutput};
+use server_api::{AppRegisterInput, AppRegisterOutput, AppTokenRenewOutput};
 use tokio::sync::{OnceCell, RwLock};
 
 use crate::test_fn::{create_app, get_url};
@@ -74,7 +74,54 @@ async fn test_1_create_app()
 }
 
 #[tokio::test]
-async fn test_2_create_app_test_fn()
+#[ignore]
+async fn test_2_update_app()
+{
+	//TODO
+}
+
+#[tokio::test]
+#[ignore]
+async fn test_3_delete_app()
+{
+	//TODO
+}
+
+#[tokio::test]
+async fn test_4_create_app_test_fn()
 {
 	create_app().await;
+
+	//TODO delete app in test fn
+}
+
+#[tokio::test]
+async fn test_5_renew_tokens()
+{
+	let app_data = create_app().await;
+
+	//TODO add here the customer jwt when customer mod is done
+
+	let url = get_url("api/v1/customer/app/".to_owned() + app_data.app_id.as_str() + "/token_renew");
+
+	let client = reqwest::Client::new();
+	let res = client.patch(url).send().await.unwrap();
+
+	assert_eq!(res.status(), StatusCode::OK);
+
+	let body = res.text().await.unwrap();
+
+	let out = ServerOutput::<AppTokenRenewOutput>::from_string(body.as_str()).unwrap();
+
+	assert_eq!(out.status, true);
+	assert_eq!(out.err_code, None);
+
+	let out = match out.result {
+		Some(v) => v,
+		None => panic!("out is not here"),
+	};
+
+	//must be new tokens
+	assert_ne!(out.secret_token, app_data.secret_token);
+	assert_ne!(out.public_token, app_data.public_token);
 }
