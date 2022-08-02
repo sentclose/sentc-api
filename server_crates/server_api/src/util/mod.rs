9fdde3d83cd73b23b065b4c73d64dd77eb569c2c
Jörn Heinemann <joernheinemann@gmx.de>
2022-08-02 21:03:36 +0200

    move app cache keys to util mod

---
+++ server_crates/server_api/src/util/mod.rs
@@ -0,0 +1,14 @@
+pub static JWT_CACHE: &'static str = "jwtcache_";
+pub static APP_TOKEN_CACHE: &'static str = "apptokencache_";
+pub static INTERNAL_GROUP_DATA_CACHE: &'static str = "internalgroupdatacache_";
+pub static INTERNAL_GROUP_USER_DATA_CACHE: &'static str = "internalgroupuserdatacache_";
+
+pub(crate) fn get_group_cache_key(app_id: &str, group_id: &str) -> String
+{
+	INTERNAL_GROUP_DATA_CACHE.to_string() + app_id + "_" + group_id
+}
+
+pub(crate) fn get_group_user_cache_key(app_id: &str, group_id: &str, user_id: &str) -> String
+{
+	INTERNAL_GROUP_USER_DATA_CACHE.to_string() + app_id + "_" + group_id + "_" + user_id
+}
