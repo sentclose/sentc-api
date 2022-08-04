f48a53be7e4bea359107ca2bbc6d479a7fa78640
Jörn Heinemann <joernheinemann@gmx.de>
2022-08-04 19:19:55 +0200

    added test for email send

---
+++ server_crates/server_api/tests/tests_1_customer.rs
@@ -0,0 +1,50 @@
+use std::env;
+
+use sentc_crypto_common::user::RegisterData;
+use sentc_crypto_common::ServerOutput;
+use server_api_common::customer::{CustomerRegisterData, CustomerRegisterOutput};
+
+use crate::test_fn::get_url;
+
+mod test_fn;
+
+#[ignore]
+#[tokio::test]
+async fn test_0_register_customer_with_email()
+{
+	//Test here with real email send -> don't do it in regular test run!
+
+	let url = get_url("api/v1/customer/register".to_string());
+
+	dotenv::dotenv().ok();
+
+	let email = env::var("EMAIL_ADDRESS_TEST").unwrap();
+
+	let register_data = sentc_crypto::user::register(email.as_str(), "12345").unwrap();
+	let register_data = RegisterData::from_string(register_data.as_str()).unwrap();
+
+	let input = CustomerRegisterData {
+		email,
+		register_data,
+	};
+
+	let public_token = env::var("SENTC_PUBLIC_TOKEN").unwrap();
+
+	let client = reqwest::Client::new();
+	let res = client
+		.post(url)
+		.header("x-sentc-app-token", public_token)
+		.body(serde_json::to_string(&input).unwrap())
+		.send()
+		.await
+		.unwrap();
+
+	let body = res.text().await.unwrap();
+
+	println!("result for registration = {}", body);
+
+	let out = ServerOutput::<CustomerRegisterOutput>::from_string(body.as_str()).unwrap();
+
+	assert_eq!(out.status, true);
+	assert_eq!(out.err_code, None);
+}
