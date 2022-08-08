3c73fa209d750d4c5be67e2ae20d1adf6f07e9c5
Jörn Heinemann <joernheinemann@gmx.de>
2022-08-08 12:19:03 +0200

    return generated key register output

--- server_crates/server_api/src/key_management/mod.rs
+++ server_crates/server_api/src/key_management/mod.rs
@@ -1,8 +1,7 @@
 use rustgram::Request;
-use sentc_crypto_common::crypto::{GeneratedSymKeyHeadServerInput, GeneratedSymKeyHeadServerOutput};
-use sentc_crypto_common::server_default::ServerSuccessOutput;
+use sentc_crypto_common::crypto::{GeneratedSymKeyHeadServerInput, GeneratedSymKeyHeadServerOutput, GeneratedSymKeyHeadServerRegisterOutput};
 
-use crate::core::api_res::{echo, echo_success, ApiErrorCodes, HttpErr, JRes};
+use crate::core::api_res::{echo, ApiErrorCodes, HttpErr, JRes};
 use crate::core::input_helper::{bytes_to_json, get_raw_body};
 use crate::core::url_helper::{get_name_param_from_params, get_name_param_from_req, get_params};
 use crate::customer_app::app_util::{check_endpoint_with_app_options, get_app_data_from_req, Endpoint};
@@ -10,7 +9,7 @@ use crate::customer_app::app_util::{check_endpoint_with_app_options, get_app_dat
 mod key_entity;
 mod key_model;
 
-pub(crate) async fn register_sym_key(mut req: Request) -> JRes<ServerSuccessOutput>
+pub(crate) async fn register_sym_key(mut req: Request) -> JRes<GeneratedSymKeyHeadServerRegisterOutput>
 {
 	let body = get_raw_body(&mut req).await?;
 	let input: GeneratedSymKeyHeadServerInput = bytes_to_json(&body)?;
@@ -18,9 +17,13 @@ pub(crate) async fn register_sym_key(mut req: Request) -> JRes<ServerSuccessOutp
 	let app_data = get_app_data_from_req(&req)?;
 	check_endpoint_with_app_options(app_data, Endpoint::KeyRegister)?;
 
-	key_model::register_sym_key(app_data.app_data.app_id.to_string(), input).await?;
+	let key_id = key_model::register_sym_key(app_data.app_data.app_id.to_string(), input).await?;
 
-	echo_success()
+	let out = GeneratedSymKeyHeadServerRegisterOutput {
+		key_id,
+	};
+
+	echo(out)
 }
 
 pub(crate) async fn get_sym_key_by_id(req: Request) -> JRes<GeneratedSymKeyHeadServerOutput>
