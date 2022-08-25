ae2a6ddd3603aa44537ece845e0dd44cc8e85a4d
Jörn Heinemann <joernheinemann@gmx.de>
2022-08-25 11:06:50 +0200

    fixed wrong route path

--- server_crates/server_api/src/routes.rs
+++ server_crates/server_api/src/routes.rs
@@ -184,7 +184,7 @@ pub(crate) fn routes() -> Router
 			.add(app_token::app_token_transform),
 	);
 	router.get(
-		"/api/v1/group/:last_fetched_time/:last_group_id",
+		"/api/v1/group/all/:last_fetched_time/:last_group_id",
 		r(crate::group::get_all_groups_for_user)
 			.add(jwt::jwt_transform)
 			.add(app_token::app_token_transform),
