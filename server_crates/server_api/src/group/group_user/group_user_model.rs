acbd82f9df523c9cbe59cc9e22e45bafc7943f1d
Jörn Heinemann <joernheinemann@gmx.de>
2022-09-06 18:52:56 +0200

    only delete real group user, not parent group

--- server_crates/server_api/src/group/group_user/group_user_model.rs
+++ server_crates/server_api/src/group/group_user/group_user_model.rs
@@ -442,9 +442,10 @@ pub(super) async fn user_leave_group(group_id: GroupId, user_id: UserId, rank: i
 	}
 
 	//language=SQL
-	let sql = "DELETE FROM sentc_group_user WHERE group_id = ? AND user_id = ?";
+	let sql = "DELETE FROM sentc_group_user WHERE group_id = ? AND user_id = ? AND type = ?";
 
-	exec(sql, set_params!(group_id, user_id)).await?;
+	//only delete normal user
+	exec(sql, set_params!(group_id, user_id, 0)).await?;
 
 	Ok(())
 }
