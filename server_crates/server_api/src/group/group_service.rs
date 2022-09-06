8f2ed0e02ea71c4bfb21f9958d1118a2dd6d6ed3
Jörn Heinemann <joernheinemann@gmx.de>
2022-09-06 18:53:53 +0200

    just return the future

--- server_crates/server_api/src/group/group_service.rs
+++ server_crates/server_api/src/group/group_service.rs
@@ -10,18 +10,16 @@ use crate::group::group_model;
 use crate::util::api_res::AppRes;
 use crate::util::get_group_cache_key;
 
-pub async fn create_group(
+pub fn create_group(
 	app_id: AppId,
 	user_id: UserId,
 	input: CreateData,
 	group_type: i32,
 	parent_group_id: Option<GroupId>,
 	user_rank: Option<i32>,
-) -> AppRes<GroupId>
+) -> impl Future<Output = AppRes<GroupId>>
 {
-	let group_id = group_model::create(app_id, user_id, input, parent_group_id, user_rank, group_type).await?;
-
-	Ok(group_id)
+	group_model::create(app_id, user_id, input, parent_group_id, user_rank, group_type)
 }
 
 pub async fn delete_group(app_id: AppId, group_id: GroupId, user_rank: i32) -> AppRes<()>
