306ded7793f8d413b66016f4e69c15901afbed33
Jörn Heinemann <joernheinemann@gmx.de>
2022-09-13 21:56:12 +0200

    added simple file serve WIP

--- server_crates/server_dashboard/server_dashboard_backend/src/lib.rs
+++ server_crates/server_dashboard/server_dashboard_backend/src/lib.rs
@@ -1,4 +1,21 @@
-use rustgram::{r, Request, Router};
+use rustgram::service::IntoResponse;
+use rustgram::{r, Request, Response, Router};
+use server_api::util::api_res::HttpErr;
+use server_core::file;
+use server_core::file::FileHandler;
+use server_core::url_helper::get_name_param_from_req;
+use tokio::sync::OnceCell;
+
+static LOCAL_FILE_HANDLER: OnceCell<Box<dyn FileHandler>> = OnceCell::const_new();
+
+pub async fn start()
+{
+	let path = "server_crates/server_dashboard/client/server_dashboard_frontend/pkg".to_string();
+
+	LOCAL_FILE_HANDLER
+		.get_or_init(move || async { file::get_local_storage(path) })
+		.await;
+}
 
 /**
 A Route to load the static files
@@ -11,9 +28,17 @@ pub fn routes(router: &mut Router)
 /**
 Load the static file from the static dir.
  */
-async fn read_file(_req: Request) -> String
+async fn read_file(req: Request) -> Response
 {
-	//TODO
+	let file = match get_name_param_from_req(&req, "path") {
+		Ok(f) => f,
+		Err(e) => return Into::<HttpErr>::into(e).into_response(),
+	};
 
-	format!("file")
+	let handler = LOCAL_FILE_HANDLER.get().unwrap();
+
+	match handler.get_part(file).await {
+		Ok(res) => res,
+		Err(e) => Into::<HttpErr>::into(e).into_response(),
+	}
 }
