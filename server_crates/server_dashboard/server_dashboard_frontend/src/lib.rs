370141d7ed6e9dfc84a7093b52be978a161b5d87
Jörn Heinemann <joernheinemann@gmx.de>
2022-09-14 12:16:21 +0200

    decode jwt

--- server_crates/server_dashboard/server_dashboard_frontend/src/lib.rs
+++ server_crates/server_dashboard/server_dashboard_frontend/src/lib.rs
@@ -12,6 +12,91 @@ use server_api_common::app::{AppFileOptions, AppOptions};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
+pub struct Claims
+{
+	aud: String,
+	sub: String, //the app id
+	exp: usize,
+	iat: usize,
+
+	//sentc
+	internal_user_id: String,
+	group_id: String,
+	device_id: String,
+	device_identifier: String,
+	fresh: bool, //was this token from refresh jwt or from login
+}
+
+#[wasm_bindgen]
+impl Claims
+{
+	pub fn get_aud(&self) -> String
+	{
+		self.aud.clone()
+	}
+
+	pub fn get_sub(&self) -> String
+	{
+		self.sub.clone()
+	}
+
+	pub fn get_exp(&self) -> usize
+	{
+		self.exp
+	}
+
+	pub fn get_iat(&self) -> usize
+	{
+		self.iat
+	}
+
+	pub fn get_internal_user_id(&self) -> String
+	{
+		self.internal_user_id.clone()
+	}
+
+	pub fn get_device_id(&self) -> String
+	{
+		self.device_id.clone()
+	}
+
+	pub fn get_group_id(&self) -> String
+	{
+		self.group_id.clone()
+	}
+
+	pub fn get_device_identifier(&self) -> String
+	{
+		self.device_identifier.clone()
+	}
+
+	pub fn get_fresh(&self) -> bool
+	{
+		self.fresh
+	}
+}
+
+impl From<sentc_crypto_full::jwt::Claims> for Claims
+{
+	fn from(claims: sentc_crypto_full::jwt::Claims) -> Self
+	{
+		Self {
+			aud: claims.aud,
+			sub: claims.sub,
+			exp: claims.exp,
+			iat: claims.iat,
+			internal_user_id: claims.internal_user_id,
+			group_id: claims.group_id,
+			device_id: claims.device_id,
+			device_identifier: claims.device_identifier,
+			fresh: claims.fresh,
+		}
+	}
+}
+
+//__________________________________________________________________________________________________
+
+#[wasm_bindgen]
 pub struct CustomerEmailData
 {
 	validate_email: bool,
@@ -108,6 +193,14 @@ impl CustomerDoneLoginOutput
 	{
 		self.user_keys.device_id.clone()
 	}
+}
+
+#[wasm_bindgen]
+pub fn decode_jwt(jwt: &str) -> Result<Claims, JsValue>
+{
+	let claims = sentc_crypto_full::jwt::decode_jwt(jwt)?;
+
+	Ok(claims.into())
 }
 
 #[wasm_bindgen]
