43d796b8300862151e3c25f2d729aceed89e0b83
Jörn Heinemann <joernheinemann@gmx.de>
2022-09-14 13:13:19 +0200

    move default closed for app options

--- server_crates/server_api_common/src/app.rs
+++ server_crates/server_api_common/src/app.rs
@@ -113,6 +113,49 @@ impl Default for AppOptions
 
 impl AppOptions
 {
+	pub fn default_closed() -> Self
+	{
+		Self {
+			group_create: 0,
+			group_get: 0,
+			group_user_keys: 0,
+			group_user_update_check: 0,
+			group_invite: 0,
+			group_auto_invite: 0,
+			group_reject_invite: 0,
+			group_accept_invite: 0,
+			group_join_req: 0,
+			group_accept_join_req: 0,
+			group_reject_join_req: 0,
+			group_key_rotation: 0,
+			group_user_delete: 0,
+			group_delete: 0,
+			group_leave: 0,
+			group_change_rank: 0,
+			user_exists: 0,
+			user_register: 0,
+			user_delete: 0,
+			user_update: 0,
+			user_change_password: 0,
+			user_reset_password: 0,
+			user_prepare_login: 0,
+			user_done_login: 0,
+			user_public_data: 0,
+			user_jwt_refresh: 0,
+			key_register: 0,
+			key_get: 0,
+			group_list: 0,
+			file_register: 0,
+			file_part_upload: 0,
+			file_get: 0,
+			file_part_download: 0,
+			user_device_register: 0,
+			user_device_delete: 0,
+			user_device_list: 0,
+			group_invite_stop: 0,
+		}
+	}
+
 	pub fn default_lax() -> Self
 	{
 		Self {
