e01bf30073bb8dd36a8a3b7eeedf91e1911bc387
Jörn Heinemann <joernheinemann@gmx.de>
2022-09-19 16:28:29 +0200

    remove dependency

--- server_crates/helper/dummy_server/src/test_external_file.rs
+++ server_crates/helper/dummy_server/src/test_external_file.rs
@@ -1,10 +1,9 @@
 use rustgram::Request;
-use sentc_crypto::sdk_common::PartId;
 use server_core::input_helper::{bytes_to_json, get_raw_body};
 use server_core::url_helper::get_name_param_from_req;
 use tokio::sync::{OnceCell, RwLock};
 
-static PART_IDS: OnceCell<RwLock<Vec<PartId>>> = OnceCell::const_new();
+static PART_IDS: OnceCell<RwLock<Vec<String>>> = OnceCell::const_new();
 
 pub(crate) async fn upload_part(req: Request) -> &'static str
 {
@@ -27,7 +26,7 @@ pub(crate) async fn delete(mut req: Request) -> &'static str
 
 	let body = get_raw_body(&mut req).await.unwrap();
 
-	let input: Vec<PartId> = bytes_to_json(&body).unwrap();
+	let input: Vec<String> = bytes_to_json(&body).unwrap();
 
 	let ids = PART_IDS
 		.get_or_init(|| async move { RwLock::new(Vec::with_capacity(502)) })
