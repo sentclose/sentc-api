ce62cdc4e282796130cb149e84db7acc8b76164b
Jörn Heinemann <joernheinemann@gmx.de>
2023-04-28 23:32:23 +0200

    added type for customer group create

--- server_crates/server_api_common/src/customer.rs
+++ server_crates/server_api_common/src/customer.rs
@@ -1,5 +1,5 @@
 use sentc_crypto_common::user::{CaptchaInput, DoneLoginLightOutput, ResetPasswordData, UserDeviceRegisterInput};
-use sentc_crypto_common::{AppId, CustomerId};
+use sentc_crypto_common::{AppId, CustomerId, GroupId};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
@@ -71,6 +71,13 @@ pub struct CustomerDonePasswordResetInput
 	pub reset_password_data: ResetPasswordData,
 }
 
+#[derive(Serialize, Deserialize)]
+pub struct CustomerGroupCreateInput
+{
+	pub name: Option<String>,
+	pub des: Option<String>,
+}
+
 //__________________________________________________________________________________________________
 
 #[derive(Serialize, Deserialize)]
@@ -111,7 +118,64 @@ impl server_core::db::FromSqliteRow for CustomerAppList
 			id: server_core::take_or_err!(row, 0),
 			identifier: server_core::take_or_err!(row, 1),
 			time: server_core::take_or_err_u128!(row, 2),
-			group_name: server_core::take_or_err!(row, 3, String),
+			group_name: server_core::take_or_err!(row, 3),
+		})
+	}
+}
+
+//__________________________________________________________________________________________________
+
+#[derive(Serialize, Deserialize)]
+pub struct CustomerGroupView
+{
+	pub data: CustomerGroupList,
+	pub apps: Vec<CustomerAppList>,
+}
+
+//__________________________________________________________________________________________________
+
+#[derive(Serialize, Deserialize)]
+pub struct CustomerGroupList
+{
+	pub id: GroupId,
+	pub time: u128,
+	pub rank: i32,
+	pub group_name: Option<String>,
+	pub des: Option<String>,
+}
+
+#[cfg(feature = "server")]
+#[cfg(feature = "mysql")]
+impl server_core::db::mysql_async_export::prelude::FromRow for CustomerGroupList
+{
+	fn from_row_opt(mut row: server_core::db::mysql_async_export::Row) -> Result<Self, server_core::db::mysql_async_export::FromRowError>
+	where
+		Self: Sized,
+	{
+		Ok(Self {
+			id: server_core::take_or_err!(row, 0, String),
+			time: server_core::take_or_err!(row, 1, u128),
+			rank: server_core::take_or_err!(row, 2, i32),
+			group_name: server_core::take_or_err_opt!(row, 3, String),
+			des: server_core::take_or_err_opt!(row, 4, String),
+		})
+	}
+}
+
+#[cfg(feature = "server")]
+#[cfg(feature = "sqlite")]
+impl server_core::db::FromSqliteRow for CustomerGroupList
+{
+	fn from_row_opt(row: &server_core::db::rusqlite_export::Row) -> Result<Self, server_core::db::FormSqliteRowError>
+	where
+		Self: Sized,
+	{
+		Ok(Self {
+			id: server_core::take_or_err!(row, 0),
+			time: server_core::take_or_err_u128!(row, 1),
+			rank: server_core::take_or_err!(row, 2),
+			group_name: server_core::take_or_err_opt!(row, 3),
+			des: server_core::take_or_err_opt!(row, 4),
 		})
 	}
 }
