cff37c300ae9483e04418ef62c8d0376eef2a176
Jörn Heinemann <joernheinemann@gmx.de>
2023-07-04 11:08:10 +0200

    added test for customer register disable

--- server_crates/server_api/tests/tests_9_customer_special.rs
+++ server_crates/server_api/tests/tests_9_customer_special.rs
@@ -1,7 +1,9 @@
 use std::env;
 
+use rustgram_server_util::error::ServerErrorCodes;
 use sentc_crypto_common::user::RegisterData;
 use sentc_crypto_common::ServerOutput;
+use server_api::util::api_res::ApiErrorCodes;
 use server_api_common::customer::{CustomerData, CustomerRegisterData, CustomerRegisterOutput};
 
 use crate::test_fn::{get_captcha, get_url};
@@ -52,4 +54,46 @@ async fn test_0_register_customer_with_email()
 
 	assert!(out.status);
 	assert_eq!(out.err_code, None);
+}
+
+#[ignore]
+#[tokio::test]
+async fn test_10_1_not_register_when_register_is_disabled()
+{
+	//only run with env CUSTOMER_REGISTER=0
+
+	let url = get_url("api/v1/customer/register".to_string());
+
+	let email = "hello@localhost.de".to_string();
+
+	let register_data = sentc_crypto::user::register(email.as_str(), "12345").unwrap();
+	let register_data = RegisterData::from_string(register_data.as_str()).unwrap();
+
+	let captcha_input = get_captcha().await;
+
+	let input = CustomerRegisterData {
+		customer_data: CustomerData {
+			name: "abc".to_string(),
+			first_name: "abc".to_string(),
+			company: None,
+		},
+		email,
+		register_data: register_data.device,
+		captcha_input,
+	};
+
+	let client = reqwest::Client::new();
+	let res = client
+		.post(url)
+		.body(serde_json::to_string(&input).unwrap())
+		.send()
+		.await
+		.unwrap();
+
+	let body = res.text().await.unwrap();
+
+	let out = ServerOutput::<CustomerRegisterOutput>::from_string(body.as_str()).unwrap();
+
+	assert!(!out.status);
+	assert_eq!(out.err_code.unwrap(), ApiErrorCodes::CustomerDisable.get_int_code());
 }
