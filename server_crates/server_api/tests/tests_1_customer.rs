1b040078323771bd8c4040d278140be180ffd415
Jörn Heinemann <joernheinemann@gmx.de>
2023-07-04 09:57:52 +0200

    move special tests to another files

--- server_crates/server_api/tests/tests_1_customer.rs
+++ server_crates/server_api/tests/tests_1_customer.rs
@@ -31,52 +31,6 @@ pub struct CustomerState
 
 static CUSTOMER_TEST_STATE: OnceCell<RwLock<CustomerState>> = OnceCell::const_new();
 
-#[ignore]
-#[tokio::test]
-async fn test_0_register_customer_with_email()
-{
-	//Test here with real email send -> don't do it in regular test run!
-
-	let url = get_url("api/v1/customer/register".to_string());
-
-	dotenv::from_filename("sentc.env").ok();
-
-	let email = env::var("EMAIL_ADDRESS_TEST").unwrap();
-
-	let register_data = sentc_crypto::user::register(email.as_str(), "12345").unwrap();
-	let register_data = RegisterData::from_string(register_data.as_str()).unwrap();
-
-	let captcha_input = get_captcha().await;
-
-	let input = CustomerRegisterData {
-		customer_data: CustomerData {
-			name: "abc".to_string(),
-			first_name: "abc".to_string(),
-			company: None,
-		},
-		email,
-		register_data: register_data.device,
-		captcha_input,
-	};
-
-	let client = reqwest::Client::new();
-	let res = client
-		.post(url)
-		.body(serde_json::to_string(&input).unwrap())
-		.send()
-		.await
-		.unwrap();
-
-	let body = res.text().await.unwrap();
-
-	println!("result for registration = {}", body);
-
-	let out = ServerOutput::<CustomerRegisterOutput>::from_string(body.as_str()).unwrap();
-
-	assert!(out.status);
-	assert_eq!(out.err_code, None);
-}
-
 #[tokio::test]
 async fn aaa_init_global()
 {
