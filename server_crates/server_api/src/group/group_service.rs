ec66e47a0be4bb9791cf1d8855adeb353167a90c
Jörn Heinemann <joernheinemann@gmx.de>
2023-07-18 15:30:26 +0200

    for normal groups, handle ext if any

--- server_crates/server_api/src/group/group_service.rs
+++ server_crates/server_api/src/group/group_service.rs
@@ -2,15 +2,17 @@ use std::future::Future;
 
 use rustgram_server_util::cache;
 use rustgram_server_util::res::AppRes;
-use sentc_crypto_common::group::{CreateData, GroupLightServerData, GroupUserAccessBy};
+use sentc_crypto_common::group::{GroupLightServerData, GroupUserAccessBy};
 use sentc_crypto_common::{AppId, GroupId, SymKeyId, UserId};
 
+use crate::ext::group::group_ext_service;
 use crate::file::file_service;
 use crate::group::group_entities::{GroupChildrenList, GroupServerData, GroupUserKeys, InternalGroupDataComplete};
 use crate::group::group_model;
-use crate::sentc_group_entities::GroupHmacData;
+use crate::sentc_group_entities::{GroupCreateData, GroupCreateDataForModel, GroupHmacData};
 use crate::sentc_user_entities::UserPublicKeyDataEntity;
 use crate::util::get_group_cache_key;
+use crate::GROUP_TYPE_NORMAL;
 
 pub fn create_group_light<'a>(
 	app_id: impl Into<AppId> + 'a,
@@ -33,27 +35,53 @@ pub fn create_group_light<'a>(
 	)
 }
 
-pub fn create_group<'a>(
-	app_id: impl Into<AppId> + 'a,
-	user_id: impl Into<UserId> + 'a,
-	input: CreateData,
+pub async fn create_group(
+	app_id: impl Into<AppId>,
+	user_id: impl Into<UserId>,
+	input: GroupCreateData,
 	group_type: i32,
 	parent_group_id: Option<GroupId>,
 	user_rank: Option<i32>,
 	connected_group: Option<GroupId>,
 	is_connected_group: bool,
-) -> impl Future<Output = AppRes<(GroupId, SymKeyId)>> + 'a
+) -> AppRes<(GroupId, SymKeyId)>
 {
-	group_model::create(
-		app_id,
+	let app_id = app_id.into();
+
+	let (group_id, first_key_id) = group_model::create(
+		&app_id,
 		user_id,
-		input,
+		GroupCreateDataForModel {
+			encrypted_group_key: input.encrypted_group_key,
+			group_key_alg: input.group_key_alg,
+			encrypted_group_key_alg: input.encrypted_group_key_alg,
+			encrypted_private_group_key: input.encrypted_private_group_key,
+			public_group_key: input.public_group_key,
+			keypair_encrypt_alg: input.keypair_encrypt_alg,
+			creator_public_key_id: input.creator_public_key_id,
+			encrypted_hmac_key: input.encrypted_hmac_key,
+			encrypted_hmac_alg: input.encrypted_hmac_alg,
+			encrypted_sign_key: input.encrypted_sign_key,
+			verify_key: input.verify_key,
+			keypair_sign_alg: input.keypair_sign_alg,
+			public_key_sig: input.public_key_sig,
+		},
 		parent_group_id,
 		user_rank,
 		group_type,
 		connected_group,
 		is_connected_group,
 	)
+	.await?;
+
+	if group_type == GROUP_TYPE_NORMAL {
+		//register also ext with the sym key id
+		if let Some(v) = input.group_ext {
+			group_ext_service::create(app_id, &group_id, &first_key_id, v).await?;
+		}
+	}
+
+	Ok((group_id, first_key_id))
 }
 
 pub async fn delete_group(app_id: &str, group_id: &str, user_rank: i32) -> AppRes<()>
@@ -105,7 +133,7 @@ pub async fn get_user_group_data(group_data: &InternalGroupDataComplete) -> AppR
 	let group_id = &group_data.group_data.id;
 	let user_id = &group_data.user_data.user_id;
 
-	let (keys, hmac_keys, key_update) = tokio::try_join!(
+	let (keys, hmac_keys, key_update, ext) = tokio::try_join!(
 		get_user_group_keys(
 			app_id, group_id, user_id, 0, //fetch the first page
 			"",
@@ -114,7 +142,8 @@ pub async fn get_user_group_data(group_data: &InternalGroupDataComplete) -> AppR
 			app_id, group_id, 0, //fetch the first page
 			"",
 		),
-		group_model::check_for_key_update(app_id, user_id, group_id)
+		group_model::check_for_key_update(app_id, user_id, group_id),
+		group_ext_service::get_ext(app_id, group_id, 0, "")
 	)?;
 
 	let (parent, access_by) = extract_parent_and_access_by(group_data);
@@ -130,6 +159,7 @@ pub async fn get_user_group_data(group_data: &InternalGroupDataComplete) -> AppR
 		joined_time: group_data.user_data.joined_time,
 		access_by,
 		is_connected_group: group_data.group_data.is_connected_group,
+		ext: Some(ext),
 	})
 }
 
